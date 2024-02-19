f9082e77bb90400cce2739372e7c27931e90f4d6
서광명 <auddlzz000@gmail.com>
2024-02-19 02:11:50 +0000

    feat - 페이지 생성

---
+++ src/app/(order)/order-details/[id]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OrderDetails = ({ params }) => {
+
+    const { id } = params;
+
+    return (
+        <div>OrderDetails {id}</div>
+    );
+}
+
+export default OrderDetails;
